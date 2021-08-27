98da4dad78f5f1b6fb5b60e00c79e6f40047ee82
stacy <stacydn23@gmail.com>
2021-08-26 20:44:53 -0400

    structure get fetch request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 let addToy = false;
+let divCollect = document.querySelector('#toy-collection')
+
+class ToyApi {
+  constructor() {
+      this.baseUrl = "http://localhost:3000"
+  }
+
+  getToys() {
+    fetch(this.baseUrl + "/toys")
+    .then(res => res.json())
+       
+  }
+
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
